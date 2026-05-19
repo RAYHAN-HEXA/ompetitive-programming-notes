91d9dec601b0d8b4a06af9051bcf29d04e2266ec
Rayhan <mohammadrayhan796@gmail.com>
2026-05-19 12:16:19 +0600

    Add source files and .gitignore improvements

---
+++ double.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+double x,y;
+printf("enther the number");
+scanf("%c %c",&x,&y);
+printf("%c %c",x,y);
+return 0;
+}
---
+++ operator.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int x,y;
+   printf("Enter two numbers: ");
+    scanf("%d %d ",&x,&y);
+    printf("%d %d",x,y);
+    return 0;
+}
+
